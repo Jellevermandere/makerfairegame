a88745fc9b89d8d449e20e8a36f0553bbe47ffb4
Jelle Vermandere <86951039+JelleKUL@users.noreply.github.com>
2022-04-15 12:03:36 +0200

    init

---
+++ Assets/Scripts/BLEDevice.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BLEDevice
+{
+    public string deviceName = "Arduino";
+    public string service = "180f";
+    public string characteristic = "2a19";
+}
