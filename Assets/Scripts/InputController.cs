1a2aa639f90a3da6e26f3fb39f0146e40172d0c1
Jelle Vermandere <45608714+Jellevermandere@users.noreply.github.com>
2022-04-22 00:11:55 +0200

    Wiimote connection

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WiimoteApi;
 
 public class InputController : MonoBehaviour
 {
     [SerializeField]
     private List<PlayerInput> playerInputs = new List<PlayerInput>();
+
+    [SerializeField]
+    private bool UseWiimotes = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +23,36 @@ public class InputController : MonoBehaviour
         GetAllplayerInputs();
     }
 
+    public void UpdateInputMethod()
+    {
+        if (UseWiimotes)
+        {
+            while (playerInputs.Count < WiimoteManager.Wiimotes.Count)
+            {
+                playerInputs.Add(new PlayerInput());
+            }
+        }
+    }
+
     void GetAllplayerInputs()
     {
-        playerInputs[0].moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (UseWiimotes)
+        {
+            for (int i = 0; i < WiimoteManager.Wiimotes.Count; i++)
+            {
+                // x = Forwards and backwards
+                // y = Left(1) and Right(-1)
+                Vector3 wiimoteAccelData = WiimoteManager.Wiimotes[i].Accel.GetAccelVector();
+                Vector2 wiimoteSteer = new Vector2(-wiimoteAccelData.y, wiimoteAccelData.x);
+
+                playerInputs[i].moveDirection = wiimoteSteer;
+            }
+        }
+        else
+        {
+            playerInputs[0].moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+        
     }
 
     /// <summary>
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    [ContextMenu("Reload Scene")]
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    [ContextMenu("Load Play Scene")]
+    public void LoadPlayScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
